9afc91431ce24209e0f3699a55c5cbd0097908b6
what-is-lav <153985079+what-is-lav@users.noreply.github.com>
2026-03-31 11:09:07 +0600

    точно последний штрих
    Updated greeting and status messages to use lowercase.

--- lesson1.py
+++ lesson1.py
@@ -4,29 +4,26 @@ class Hero:
         self.hp = hp
         self.lvl = lvl
         self.strength = strength
-
     def greet(self):
-        print(f"Привет, я {self.name}, мой уровень {self.lvl}")
-
+        print(f"привет, я {self.name}, мой уровень {self.lvl}")
     def attack(self):
         print(f"{self.name} наносит удар!")
         self.strength -= 1
-
     def rest(self):
-        print(f"{self.name} отдыхает…")
+        print(f"{self.name} отдыхает...")
         self.hp += 1
 
 kirito = Hero("Кирито", 100, 5, 100)
 asuna = Hero("Асуна", 100, 4, 95)
 kirito.greet()
-print(f"Сила до атаки: {kirito.strength}")
+print(f"сила до атаки: {kirito.strength}")
 kirito.attack()
-print(f"Сила после атаки: {kirito.strength}")
+print(f"сила после атаки: {kirito.strength}")
 print(f"HP до отдыха: {kirito.hp}")
 kirito.rest()
 print(f"HP после отдыха: {kirito.hp}")
 asuna.greet()
-print(f"Сила до атаки: {asuna.strength}")
+print(f"сила до атаки: {asuna.strength}")
 asuna.attack()
 print(f"Сила после атаки: {asuna.strength}")
 print(f"HP до отдыха: {asuna.hp}")
